6a96550218b960ca5e79c06aceb9a986555b4eb9
Halil Şafak KILIÇ <halilsafakkilic@gmail.com>
2022-04-11 00:33:52 +0300

    Dotnet version upgraded from core 5 to 6.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,119 @@
-using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using netCorePlayground.Infra;
+using netCorePlayground.Middlewares;
 
-namespace netCorePlayground
-{
-    public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddCors();
+
+builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
     {
-        public static void Main(string[] args)
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"])),
+            ClockSkew = TimeSpan.Zero,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true
+        };
+    });
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+builder.Services.AddAuthorization((Action<Microsoft.AspNetCore.Authorization.AuthorizationOptions>) (config =>
+{
+    config.AddPolicy(Policies.UserPolicy, Policies.UserPolicyGenerator());
+
+    config.AddPolicy(Policies.AdminRole, Policies.AdminRoleGenerator());
+    config.AddPolicy(Policies.TesterUserRole, Policies.TesterUserRoleGenerator());
+}));
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo {Title = "Weather Forecast API", Version = "v1"});
+
+    string actionSummaryXMLFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, actionSummaryXMLFile), true);
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description =
+            "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter your access token in the text input below.\r\n\r\nExample: \"Bearer aBcDDE.MyToken\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
+builder.Services.AddSwaggerGenNewtonsoftSupport();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseMiddleware<ExceptionMiddleware>();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1"); });
+
+app.UseHttpsRedirection();
+
+var corsAllowedOrigins = app.Configuration.GetSection("CorsAllowedOrigins").Get<List<string>>();
+app.UseCors(options =>
+{
+    options.WithOrigins(corsAllowedOrigins.ToArray())
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+});
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+app.Run();
